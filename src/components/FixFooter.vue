0fa3346bc48d9a0e6eb39123240653dd2c49b6dd
Guitar <110451739+4arcane@users.noreply.github.com>
2024-08-21 16:36:34 +0700

    upload 1st

---
+++ src/components/FixFooter.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="fix_footer">
+        <a href="/"><span class="link_ico ico_1  "><i class="fas fa-home "></i> </span>
+            <strong>หน้าหลัก</strong></a>
+        <a href="https://app.linexxbet.com/auth"><span class="link_ico ico_1   "><i class="fas fa-sign-in-alt"></i></span>
+            <strong>เข้าสู่ระบบ</strong></a>
+        <a href="https://app.linexxbet.com/register?ref=8fe0093bb30d6f8c31474bd0764e6ac0"><span class="link_ico ico_2   "><i class="fas fa-user-plus"></i></span>
+            <strong>สมัครสมาชิก</strong></a>
+        <a href="/โปรโมชั่น"><span class="link_ico ico_2"><i class="fas fa-gift"></i></span>
+            <strong>โปรโมชั่น</strong></a>
+        <a class="last" href="https://line.me/R/ti/p/@147ucewm?oat_content=url&ts=06181339"><span class="link_ico ico_5"> <i class="  fas fa-phone"></i>
+            </span><strong> ติดต่อเรา</strong></a>
+    </div>
+</template>
